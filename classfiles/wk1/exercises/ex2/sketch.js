b31003dca8ab2a7410f4d823140ae47ad932aa80
Jessica Montoya <jmontoya@mcad.edu>
2022-01-19 22:14:52 -0600

    practice making an arc

--- classfiles/wk1/exercises/ex2/sketch.js
+++ classfiles/wk1/exercises/ex2/sketch.js
@@ -1,12 +1,8 @@
-function setup() {
+function setup () {
   createCanvas(480, 120);
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function draw () {
+  background (204);
+  arc (90, 60, 80, 80, 0, HALF_PI);
 }
