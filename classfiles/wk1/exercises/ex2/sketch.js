baa520d7f35a2a260e976f69dbc197556b7d7b6d
Jessica Montoya <jmontoya@mcad.edu>
2022-01-20 18:11:34 -0600

    fixed auto-fixable problems

--- classfiles/wk1/exercises/ex2/sketch.js
+++ classfiles/wk1/exercises/ex2/sketch.js
@@ -1,14 +1,14 @@
-function setup() {
+function setup () {
   createCanvas(400, 400);
-  noStroke ();
+  noStroke();
 }
 
 function draw () {
   if (mouseIsPressed) {
-    fill (0, 15, 46);
+    fill(0, 15, 46);
     ellipse(mouseX, mouseY, 80, 80);
   } else {
     fill(180, 240, 0);
-    rect (mouseX, mouseY, 80, 80);
+    rect(mouseX, mouseY, 80, 80);
   }
 }
