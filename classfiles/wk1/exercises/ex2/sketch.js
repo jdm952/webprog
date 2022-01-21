69589a4ac92792ee9fdd793891f5f8f64a24114e
Jessica Montoya <jmontoya@mcad.edu>
2022-01-20 18:10:53 -0600

    wk1, ex2

--- classfiles/wk1/exercises/ex2/sketch.js
+++ classfiles/wk1/exercises/ex2/sketch.js
@@ -1,8 +1,14 @@
-function setup () {
-  createCanvas(480, 120);
+function setup() {
+  createCanvas(400, 400);
+  noStroke ();
 }
 
 function draw () {
-  background (204);
-  arc (90, 60, 80, 80, 0, HALF_PI);
+  if (mouseIsPressed) {
+    fill (0, 15, 46);
+    ellipse(mouseX, mouseY, 80, 80);
+  } else {
+    fill(180, 240, 0);
+    rect (mouseX, mouseY, 80, 80);
+  }
 }
