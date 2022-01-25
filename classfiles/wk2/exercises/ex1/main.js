37c2aea02f21392bc7cbd155b5220dc293118b73
Jessica Montoya <jmontoya@mcad.edu>
2022-01-24 21:52:07 -0600

    added doctypes, started ex1, wk2

---
+++ classfiles/wk2/exercises/ex1/main.js
@@ -0,0 +1,3 @@
+let myArr = ['Spy', 'Lord of the Rings', 'My Big Fat Greek Wedding', 'Strictly Ballroom', 'The Descendants'];
+console.log(myArr);
+let favMovie = prompt('what is your favorite movie?');
