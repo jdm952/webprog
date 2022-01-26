2a0a81a7942e88c7797df501a4a2a6495055593d
Jessica Montoya <jmontoya@mcad.edu>
2022-01-26 14:15:27 -0600

    adjusted ex1

--- classfiles/wk2/exercises/ex1/main.js
+++ classfiles/wk2/exercises/ex1/main.js
@@ -1,4 +1,4 @@
 let myMovies = ['Spy', 'Lord of the Rings', 'Amelie', 'Strictly Ballroom', 'The Descendants'];
 let favMovie = window.prompt('what is your favorite movie?');
 myMovies.push(favMovie);
-console.log('our favorite movies', myMovies.toString());
+console.log('our favorite movies are', myMovies.toString());
