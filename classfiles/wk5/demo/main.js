9b92dd98f7ebe447ab545e9442dc3e044b9689be
Jessica Montoya <jmontoya@mcad.edu>
2022-02-15 16:04:15 -0600

    continue working through demos

--- classfiles/wk5/demo/main.js
+++ classfiles/wk5/demo/main.js
@@ -1,13 +1,19 @@
 const DOWN = 'down';
 const UP = 'up';
 let startingX = 100;
-let startingY = 100;
+let startingY = 300;
 let cards = [];
 const gameState = {
-
-}
+    totalPairs: 8,
+    flippedCards: [],
+    numMatched: 0,
+    attempts: 0,
+    waiting: false
+};
 let cardfaceArray = [];
 let cardback;
+
+//load in images
 function preload () {
     cardback = loadImage('img/card_back200x200.png');
     cardfaceArray = [
@@ -16,13 +22,17 @@ function preload () {
         loadImage('img/img3.png'),
         loadImage('img/img4.png'),
         loadImage('img/img5.png'),
+        loadImage('img/img6.png'),
+        loadImage('img/img7.png'),
+        loadImage('img/img8.png'),
     ]
 }
+
+//set up canvas and cards
 function setup () {
-    createCanvas(1300, 600);
-    background(0);
+    createCanvas(1300, 1500);
     let selectedFaces = [];
-    for (let z = 0; z < 5; z++) {
+    for (let z = 0; z < 8; z++) {
         const randomIdx = floor(random(cardfaceArray.length));
         const face = cardfaceArray[randomIdx];
         selectedFaces.push(face);
@@ -31,10 +41,10 @@ function setup () {
         cardfaceArray.splice(randomIdx, 1);
     }
     selectedFaces = shuffleArray(selectedFaces);
-    for (let j = 0; j < 2; j++) {
-        for (let i = 0; i < 5; i++) {
-            const faceImage = selectedFaces.pop();
-            cards.push(new Card(startingX, startingY, faceImage));
+    for (let j = 0; j < 4; j++) {
+        for (let i = 0; i < 4; i++) {
+            const cardFaceImg = selectedFaces.pop();
+            cards.push(new Card(startingX, startingY, cardFaceImg));
             startingX += 220;
         }
         startingY += 250
@@ -43,14 +53,65 @@ function setup () {
 
 }
 
+function draw () {
+    background (0);
+    if (gameState.numMatched === gameState.totalPairs) {
+        fill('yellow');
+        textSize(66);
+        text('you win!!!', 400, 100);
+        noLoop();
+    }
+    for (let b = 0; b < cards.length; b++) {
+        if(!cards[b].isMatch) {
+            cards[b].face = DOWN;
+        }
+        cards[b].show();
+    }
+    noLoop();
+    //create scoreboard
+    gameState.flippedCards.length = 0;
+    gameState.waiting = false;
+    fill(255);
+    textSize(36);
+    text('attempts ' + gameState.attempts, 100, 100);
+    text('matches ' + gameState.numMatched, 100, 150);
+}
+
+
 function mousePressed () {
+    if (gameState.waiting) {
+        return;
+    }
     for (let k = 0; k < cards.length; k++) {
-        if(cards[k].didHit(mouseX, mouseY)) {
+        //first check flipped cards length, and then we can trigger the flip
+        if(gameState.flippedCards.length < 2 && cards[k].didHit(mouseX, mouseY)) {
             console.log('flipped', cards[k]);
+            gameState.flippedCards.push(cards[k]);
+        }
+    }
+    if(gameState.flippedCards.length === 2) {
+        gameState.attempts++;
+        if (gameState.flippedCards[0].cardFaceImg === gameState.flippedCards[1].cardFaceImg) {
+            //cards match! time to score
+            //mark cards as matched so they don't flip back
+            gameState.flippedCards[0].isMatch = true;
+            gameState.flippedCards[1].isMatch = true;
+            //empty the flipped cards array
+            gameState.flippedCards.length = 0;
+            //increment the score
+            gameState.numMatched++;
+            loop();
+        } else {
+            gameState.waiting = true;
+            const loopTimeOut = window.setTimeout(() => {
+                loop();
+                window.clearTimeout(loopTimeout);
+            }, 1000)
         }
     }
 }
 
+//create cards and flipping
 class Card {
     constructor (x, y, cardFaceImg) {
         this.x = x;
@@ -59,18 +120,19 @@ class Card {
         this.height = 200;
         this.face = DOWN;
         this.cardFaceImg = cardFaceImg;
+        this.isMatch = false;
         this.show();
     }
 
     show () {
-        if (this.face === DOWN) {
-            fill('rgb(57.7%, 9.9%, 9.9%)');
-            rect(this.x, this.y, this.width, this.height);
-            image(cardback, this.x, this.y);
-        } else {
+        if (this.face === UP || this.isMatch) {
             fill('#aaa');
             rect(this.x, this.y, this.width, this.height);
             image(this.cardFaceImg, this.x, this.y);
+        } else {
+            fill('rgb(57.7%, 9.9%, 9.9%)');
+            rect(this.x, this.y, this.width, this.height);
+            image(cardback, this.x, this.y);
         }
     }
 
