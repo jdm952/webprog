f7bbb32addb845a7c47863612fb846f8cd3021b0
Jessica Montoya <jmontoya@mcad.edu>
2022-03-05 16:36:07 -0600

    wk7 assignment

--- classfiles/wk7/assignment/main.js
+++ classfiles/wk7/assignment/main.js
@@ -1,8 +1,52 @@
-const vm = new Vue( {
-    el: "#llamas",
-    data: {
-        factsArray: ["Llamas can fly", "Llamas have six feet", "Llamas are orange", "Llamas can roar", "Llamas are super tall", "Llamas eat other llamas", "Llamas know how to party", "Llamas can read", "Llamas can talk"],
-    habitat: "ocean",
-    extra: '',
+Vue.createApp({
+    data() {
+        return {
+            flowers: [
+                {
+                    name: "Black-Eyed Susan",
+                    type: "Perennial",
+                    color: "Yellow",
+                    height: "Medium",
+                    image: "img/blackeyedsusan.jpg" 
+                },
+                {
+                    name: "Coneflower",
+                    type: "Perennial",
+                    color: "Purple",
+                    height: "Medium",
+                    image: "img/coneflower.jpg" 
+                },
+                {
+                    name: "Marigold",
+                    type: "Annual",
+                    color: "Multicolor",
+                    height: "Short",
+                    image: "img/marigold.jpg"
+                },
+                {
+                    name: "Shasta Daisy",
+                    type: "Perennial",
+                    color: "White",
+                    height: "Medium",
+                    image: "img/daisy.jpg"
+                },
+                {
+                    name: "Sunflower",
+                    type: "Annual",
+                    color: "Yellow",
+                    height: "Tall",
+                    image: "img/sunflower.jpg"
+                },
+                {
+                    name: "Zinnia",
+                    type: "Annual",
+                    color: "Multicolor",
+                    height: "Medium",
+                    image: "img/zinnia.jpg"
+                }
+            ]
+        }
     }
-})
+}).mount("#app");
+
+
