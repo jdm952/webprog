60611fccd32a48336ecb10a640246a4121007773
Jessica Montoya <jmontoya@mcad.edu>
2022-03-23 20:21:31 -0500

    wk9 ex1

---
+++ classfiles/wk9/exercises/computed_props/lib/script.js
@@ -0,0 +1,15 @@
+Vue.createApp({
+  data() {
+    return {
+      streetAddress: "2501 Stevens Ave",
+      city: "Minneapolis",
+      state: "Minnesota",
+      postalCode: "55404"
+    }
+  },
+  computed: {
+    fullAddress() {
+      return this.streetAddress + ", " + this.city + ", " + this.state + " " + this.postalCode
+    }
+  }
+}).mount("#app");
